b177dccadfef606214dc2688f3036241dfcc2cac
Huberdoggy <huberdoggy@gmail.com>
2022-06-15 11:45:12 -0500

    finished testing exponential growth. Changed int to unsigned long long to hold massive values on the latter iterations of the loop

--- Exponential-Growth/Exponential-Growth.cpp
+++ Exponential-Growth/Exponential-Growth.cpp
@@ -4,9 +4,7 @@ using namespace std;
 int main()
 {
 	const int boardSize{ 64 };
-	int rice = 1;
-	double doubleRice{}; // For use with the REALLY large nums
-	bool call_OneThousand = true, call_OneMillion = true, call_OneBillion = true; // Flags to filter output while I'm testing, to break on different points
+	unsigned long long rice{ 1 };  // For use with the REALLY large nums. Start at one since in the story, square 1 gets 1 grain
 
 	vector<int> chessboard;
 	// Since the first square will contain 2^0, we can find the amount of rice for any given square with formula 2^(n-1)
@@ -14,27 +12,12 @@ int main()
 	for (int i = 0; i < boardSize; i++)
 	{
 		chessboard.push_back(rice);
-		rice *= 2; // Push 1 grain on square 1, and then multiply by 2 for each next square
-		if (rice >= 1000 && call_OneThousand) {
-			cout << "\nNumber of rice grains at square " << i + 2 << ": " << rice; // i + 2 due to the fact I'm breaking as soon as it hits condition and I'm zero indexing.
-			call_OneThousand = false;
+		if (i >= 1)
+		{
+			rice *= 2;
 		}
-		else if (i == boardSize - 1) {
-			cout << "\nNumber of rice grains on the FINAL square: " << doubleRice; // Because there really are only 64 squares. Can't even properly store a num this big..
-		}
-		else if (rice >= 1000000 && call_OneMillion) {
-			cout << "\nNumber of rice grains at square " << i + 2 << ": " << rice;
-			doubleRice = double(rice);
-			call_OneMillion = false;
-		}
-		else if (rice >= 1000000000 && call_OneBillion) {
-			cout << "\nNumber of rice grains at square " << i + 2 << ": " << rice;
-			call_OneBillion = false;
-		}
-
-
+		cout << "\nNumber of rice grains at square " << i + 1 << ": " << rice; // i + 1 due to the fact I'm zero indexing the loop.
 	}
-
 
 	return 0;
 }
