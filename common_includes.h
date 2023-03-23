a643e4f2edb229bab29176f71bba0878765ca06c
Huberdoggy <huberdoggy@gmail.com>
2023-03-22 19:55:12 -0500

    redoing worktree using bare branch

--- common_includes.h
+++
@@ -1,9 +0,0 @@
-/*
-Kyle made this header to include most common libraries needed with his projects.
-*/
-
-#pragma once
-#include <iostream>
-#include <regex>
-#include <string>
-#include <cmath>
--- input_regex.h
+++
@@ -1,7 +0,0 @@
-#include <common_includes.h>
-# pragma once
-
-bool isValidEmail(const std::string_view s, const std::regex er) {
-	return std::regex_match(static_cast<std::string>(s), er) ? true : false;
-	// Don't return a 'string_view' from func. Bad practice
-}
