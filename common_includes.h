2156315c42fc7edd09b3522259a78ae835538647
Huberdoggy <huberdoggy@gmail.com>
2023-03-22 19:32:28 -0500

    readding files after doing some HEAD reset corrections

---
+++ common_includes.h
@@ -0,0 +1,9 @@
+/*
+Kyle made this header to include most common libraries needed with his projects.
+*/
+
+#pragma once
+#include <iostream>
+#include <regex>
+#include <string>
+#include <cmath>
---
+++ height_calc.h
@@ -0,0 +1,7 @@
+#pragma once
+
+double heightCalc(double m, double g);
+void printResult(int seconds, double nm);
+bool validateInput(const std::string u_imp, const std::regex re);
+  
+
