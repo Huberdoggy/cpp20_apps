3a79316fdfb8a8d470b65fd7759fdad6b57bbbed
Huberdoggy <huberdoggy@gmail.com>
2023-05-02 14:19:44 -0500

    modified challenge instructions, since I am now dynamically generating new numbers each run of the program

--- Experiments/Starting_Out_Book/Exercises/Ch8/Lottery_Winners/Lottery_Winners.cpp
+++ Experiments/Starting_Out_Book/Exercises/Ch8/Lottery_Winners/Lottery_Winners.cpp
@@ -4,14 +4,7 @@ A lottery ticket buyer purchases 10 tickets a week, always playing the same
 or a vector with these numbers and then lets the player enter this week’s
 winning 5-digit number. The program should perform a linear search through the
 list of the player’s numbers and report whether or not one of the tickets is a
-winner this week. Here are the numbers:
-
-13579 26791 26792 33445 55555
-62483 77777 79422 85647 93121
-
-Modify the program you wrote so it
-performs a binary search instead of a linear search.
-
+winner this week.
 */
 
 #include "common_includes.h"
